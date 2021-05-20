885edc636f54dc785ecc6cdb15363426411f50cf
Ittipol Jannapast <anacondong@Ittipols-MacBook-Pro.local>
2021-05-20 17:09:54 +0700

    update custom layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,27 @@ import { AppComponent } from './app.component';
 import {NgcCookieConsentModule, NgcCookieConsentConfig} from 'ngx-cookieconsent';
 import {TranslateModule} from '@ngx-translate/core';
 
+// defult config
+// const cookieConfig:NgcCookieConsentConfig = {
+//   cookie: {
+//     domain: 'localhost' // or 'your.domain.com' // it is mandatory to set a domain, for cookies to work properly (see https://goo.gl/S2Hy2A)
+//   },
+//   palette: {
+//     popup: {
+//       background: '#000'
+//     },
+//     button: {
+//       background: '#f1d600'
+//     }
+//   },
+//   theme: 'edgeless',
+//   type: 'opt-out'
+// };
+
+// custom layout
 const cookieConfig:NgcCookieConsentConfig = {
   cookie: {
-    domain: 'localhost' // or 'your.domain.com' // it is mandatory to set a domain, for cookies to work properly (see https://goo.gl/S2Hy2A)
+    domain: 'localhost'// it is recommended to set your domain, for cookies to work properly
   },
   palette: {
     popup: {
@@ -17,7 +35,32 @@ const cookieConfig:NgcCookieConsentConfig = {
     }
   },
   theme: 'edgeless',
-  type: 'opt-out'
+  type: 'opt-out',
+  layout: 'my-custom-layout',
+  layouts: {
+    "my-custom-layout": '{{messagelink}}{{compliance}}'
+  },
+  elements:{
+    messagelink: `
+    <span id="cookieconsent:desc" class="cc-message">{{message}} 
+      <a aria-label="learn more about cookies" tabindex="0" class="cc-link" href="{{cookiePolicyHref}}" target="_blank">{{cookiePolicyLink}}</a>, 
+      <a aria-label="learn more about our privacy policy" tabindex="1" class="cc-link" href="{{privacyPolicyHref}}" target="_blank">{{privacyPolicyLink}}</a> and our 
+      <a aria-label="learn more about our terms of service" tabindex="2" class="cc-link" href="{{tosHref}}" target="_blank">{{tosLink}}</a>
+    </span>
+    `,
+  },
+  content:{
+    message: 'By using our site, you acknowledge that you have read and understand our ',
+    
+    cookiePolicyLink: 'Cookie Policy',
+    cookiePolicyHref: 'https://cookie.com',
+
+    privacyPolicyLink: 'Privacy Policy',
+    privacyPolicyHref: 'https://privacy.com',
+
+    tosLink: 'Terms of Service',
+    tosHref: 'https://tos.com',
+  }
 };
 
 
@@ -33,4 +76,5 @@ const cookieConfig:NgcCookieConsentConfig = {
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
